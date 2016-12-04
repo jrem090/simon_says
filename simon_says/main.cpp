e59f9911c3cd9cbe86c48d46dd13b2c4f7bd69fd
DESKTOP-QTJ50D4\John <john.remmes@richmond.edu>
2016-12-04 16:11:48 -0500

    Added demo image for webpage

--- simon_says/main.cpp
+++ simon_says/main.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+/**
+ * @brief qMain
+ * Main for the application. creates the window
+ */
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
